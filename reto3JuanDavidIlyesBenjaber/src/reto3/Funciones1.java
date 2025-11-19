60662a8963f5d0a50366eb12820888fa7d3c8b9c
JuanDi240 <1DAM@A11-PC30>
2025-11-19 13:59:56 +0100

    comit3

--- reto3JuanDavidIlyesBenjaber/src/reto3/Funciones1.java
+++ reto3JuanDavidIlyesBenjaber/src/reto3/Funciones1.java
@@ -1,5 +1,58 @@
 package reto3;
 
-public class Funciones1 {
+import java.util.Scanner;
 
+public class Funciones1 {
+	
+public static void menu1(Scanner sc) {
+		System.out.println("Selecciona que haras: \n"
+				+ "1. Opcion1 \n"
+				+ "2. Opcion2");
+		int seleccion = Integer.parseInt(sc.nextLine());
+		switch (seleccion){
+		
+	
+		case 1:
+
+		
+		String nombre = "a";
+		int nombreMasPequenoNUM = Integer.MAX_VALUE;
+		String nombreMasPequeno = "Z";
+		int contadorNombrePequeno = 0;
+		String nombre_alfabeticamente_primero = "";
+		
+		try {
+		do
+		{
+			//Primera_parte
+			System.out.print("Dime un nombre: ");
+			nombre = sc.nextLine();
+			int ultimaLetra = nombre.length();
+			String primeraLetra = nombre.substring(0,1);
+			System.out.println(primeraLetra.toUpperCase() + nombre.substring(1,ultimaLetra));
+			System.out.println();
+			
+			//Segunda_parte(A)
+			if(nombre.length() < nombreMasPequenoNUM && contadorNombrePequeno == 0) {
+				nombreMasPequeno = nombre;
+				contadorNombrePequeno++;
+			}
+			
+			//Tercera_parte(B)
+			if(nombre_alfabeticamente_primero.compareTo(nombre) > 0) {
+				nombre_alfabeticamente_primero = nombre;
+			}
+			
+			
+		}while(nombre != "");
+		} catch (Exception e) {
+		}
+		
+		System.out.println();
+		System.out.println("El nombre mas queño a sido: " + nombreMasPequeno);
+		System.out.println("El nombre alfabeticamente primero es: " + nombre_alfabeticamente_primero);
+		break;
+		
+		}
+}
 }
--- reto3JuanDavidIlyesBenjaber/src/reto3/Reto3.java
+++ reto3JuanDavidIlyesBenjaber/src/reto3/Reto3.java
@@ -1,10 +1,14 @@
 package reto3;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Reto3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		
+		Funciones1.menu1(sc);
 	}
 
 }
